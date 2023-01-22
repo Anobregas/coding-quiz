aa79d2365bce1552af2cd01416320d8de22345b8
Austin Nobregas <austinnobregas@gmail.com>
2023-01-22 11:05:07 -0500

    file layout

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+var section = document.querySelector("section")
+var choice = document.querySelectorAll(".choice")
+var question = document.querySelector(".question")
+var btn = document.querySelector("button")
+var AChoice = document.getElementById("A")
+var BChoice = document.getElementById("B")
+var CChoice = document.getElementById("C")
+var presentPage = 0
+var grade = 0
+var quizText = [
+    {question: "which of the folllowing can not be used to identify a variable in JavaScript",
+        A: "let",
+        B: "const",
+        C: "give",
+        Answer: "c",
+    },
+    {question: "Which of the following is a strict comparison",
+        A: "<",
+        B: "==",
+        C: "===",
+        Answer: "c",
+    },
+    {question: "which of the folllowing can not be used to identify a variable in JavaScript",
+        A: "let",
+        B: "const",
+        C: "give",
+        Answer: "b",
+    },
+    
+]
+
+run()
+
+function run(){
+    
+}
+
+
+
+
