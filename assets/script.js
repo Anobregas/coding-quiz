5603825d66a3026e27f56ae722c92b502f884dd9
Anobregas <85459022+Anobregas@users.noreply.github.com>
2023-01-27 22:35:59 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,141 @@
-var section = document.querySelector("section")
-var choice = document.querySelectorAll(".choice")
-var question = document.querySelector(".question")
-var btn = document.querySelector("button")
-var AChoice = document.getElementById("A")
-var BChoice = document.getElementById("B")
-var CChoice = document.getElementById("C")
-var presentPage = 0
-var grade = 0
+// reached into the html and grabbed a bunch of elements
+var section = document.getElementById("quiz")
+var choiceEl = document.querySelectorAll(".choice")
+var questionEl = document.querySelector("#question")
+var btn = document.querySelector("#button")
+var AChoice = document.getElementById("first-choice")
+var BChoice = document.getElementById("second-choice")
+var CChoice = document.getElementById("third-choice")
+var currentQuiz = 0
+var score = 0
+var timer;
+var timerCount = 10
+var timerElement = document.querySelector(".timer");
+var startBtn = document.querySelector(".start-btn")
+
+// defined a list of questions
 var quizText = [
-    {question: "which of the folllowing can not be used to identify a variable in JavaScript",
+    {
+        question: "which of the folllowing can not be used to identify a variable in JavaScript",
         A: "let",
         B: "const",
         C: "give",
-        Answer: "c",
+        choice: "c",
     },
-    {question: "Which of the following is a strict comparison",
+    {
+        question: "Which of the following is a strict comparison",
         A: "<",
         B: "==",
         C: "===",
-        Answer: "c",
+        choice: "c",
     },
-    {question: "which of the folllowing can not be used to identify a variable in JavaScript",
-        A: "let",
-        B: "const",
-        C: "give",
-        Answer: "b",
+    {
+        question: "which of the folllowing is used to describe a Paragraph in HTML",
+        A: "<paragraph>",
+        B: "<p>",
+        C: "<article>",
+        choice: "b",
     },
-    
+
+
 ]
 
-run()
 
-function run(){
-    
+
+
+// keep track of the current question with current quiz
+
+
+loadquiz()
+
+function loadquiz() {
+    console.log("1")
+
+    // clear old question
+    deselectanswer()
+
+    // get the data for the current question
+    const currentQuizData = quizText[currentQuiz]
+    console.log(questionEl)
+    console.dir(questionEl)
+    questionEl.innerText = currentQuizData.question;
+    // put the answers from the question in the anser choic
+    AChoice.innerText = currentQuizData.A
+    BChoice.innerText = currentQuizData.B
+    CChoice.innerText = currentQuizData.C
+
 }
 
+function deselectanswer() {
+    console.log("2")
+    // loop over my radio button and set the value to false
+    choiceEl.forEach(choice => choice.checked = false)
+}
 
+function getselected() {
+    console.log("3")
+    // grab the radio button labels
+    choiceEl.forEach(choice => {
+        console.log(choice);
+        console.dir(choice);
+        // if the label you have has a prop called checked that is true
+        if (choice.checked) {
+            // assign the id of that label to answer
+            answer = choice.id
+        }
+    })
+    // send it back to the function that called for it
+    return answer
+}
+
+btn.addEventListener("click", () => {
+    // find out which button was selected
+    const answer = getselected()
+    // if the answer was selected
+    if (answer) {
+        console.log(answer.toLowerCase())
+        console.log(quizText[currentQuiz].choice.toLowerCase())// if the answer matches the current quiz
+        if (answer.toLowerCase() == quizText[currentQuiz].choice.toLowerCase()) {
+            score++ // infcrement the score
+        }
+
+        currentQuiz++
+
+        // check to see if you're done w/ the quiz
+        if (currentQuiz < quizText.length) {
+            // if not loadquiz
+            loadquiz()
+        } else {
+            section.innerHTML = score;
+        }
+    }
+})
+
+//start timer
+function startTimer() {
+    //
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+       
+        }
+        // Tests if time has run out
+        if (timerCount == 0) {
+            clearInterval(timer);
+            loseGame();
+        }
+
+    }, 1000);
+
+}
+function loseGame() {
+    section.textContent = "GAME OVER";
+  }
+
+
+
+
+  
 
 
